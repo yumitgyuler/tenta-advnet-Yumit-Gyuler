f43c35ddeef84410b1493c12214ac83e24349282
Yumit Gyuler <yumit.gyuler@gmail.com>
2021-04-08 16:46:02 +0200

    Added UI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,6 +8,11 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+
+            //Console.Write("How many days do you want the simulation to take?: ");
+            //int days = int.Parse(Console.ReadLine());
+            //Console.Write("How fast do you want the simulation to progress?(Tics per second): ");
+            //int ticksPerSecond = int.Parse(Console.ReadLine());
             Tick tick = new Tick(1, 3);
             //Simulator simulator = new Simulator();
 
